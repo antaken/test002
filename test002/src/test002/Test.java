ca492f44bfcdf3b9cadae8bde4bada4b74c92e14
f0416001 <f0416001@f0416001-pc>
2017-10-23 13:15:44 +0900

    First Commit

---
+++ test002/src/test002/Test.java
@@ -0,0 +1,22 @@
+package test002;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Input Number > ");
+		int n = Integer.parseInt(scan.next());
+		System.out.println("Number = "+n);
+		
+		System.out.println("Input RealNumber > ");
+		double m = Double.parseDouble(scan.next());
+		System.out.println("RealNumber = "+m);
+		
+		scan.close();
+	}
+
+}
